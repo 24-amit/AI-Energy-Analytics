e8617030cbc44ec313ec166ea3ec387764424108
Amit <amitjangra2004@gmail.com>
2026-07-26 16:15:50 +0530

    Testing for input label name

--- frontend/src/components/PredictionForm.jsx
+++ frontend/src/components/PredictionForm.jsx
@@ -97,7 +97,9 @@ function PredictionForm({ onPredictionSuccess }) {
           <div className="row g-4">
             {Object.keys(formData).map((field) => (
               <div className="col-md-3" key={field}>
-                <label className="form-label fw-semibold">{fieldLabels[field]}</label>
+                <label className="form-label fw-semibold text-danger">
+                  TEST LABEL
+                </label>
 
                 <input
                   className="form-control"
